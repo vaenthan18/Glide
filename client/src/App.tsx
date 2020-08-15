fb638b93aa6c5a42b5a0210449614119b019302e
Anoush <anoushras21@gmail.com>
2020-08-15 15:40:37 -0400

    brought app and index into branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TaskBox from './TaskBox'
+
 
 function App() {
   return (
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
-    <TaskBox name={'Anoush'} className={'x'} dueDate={'Aug 17th, 2020'} classColour={'Blue'}/>
+    <div>
+      Hello World
+    </div>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import * as serviceWorker from './serviceWorker';
+
+library.add(faPlus);
 
 ReactDOM.render(
   <React.StrictMode>
