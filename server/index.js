fd8a874dcc79dd3171e1d168606bfa2356b54abd
Vaenthan Jeevarajah <vaenthan.j@gmail.com>
2020-08-15 14:19:12 -0400

    Added PATCH endpoints, not complete yet

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(cors());
 
 // ROUTES //
 
-//Display an assignment
+// POST Requests
+
 app.post('/students', async(req, res) => {
     try{
         const { full_name, school, grade, total_points } = req.body;
@@ -92,6 +93,8 @@ app.post('/classes/:class_id/assignments', async(req, res) => {
     }
 })
 
+// GET Requests //
+
 app.get('/students/:student_id/assignments', async(req, res) => {
     try {   
         const student_id = req.params.student_id;
@@ -163,6 +166,70 @@ app.get('/classes/:class_id/students', async(req, res) => {
             [class_id]
         );
         res.json(all_assignments.rows);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+
+// PATCH Requests //
+
+app.patch('/students', async(req, res) => {
+    try{
+        const { full_name, school, grade, total_points } = req.body;
+        const new_student = await pool.query("UPDATE students (full_name, school, grade, total_points) VALUES ($1, $2, $3, $4) WHERE full_name = $1 RETURNING *",
+            [full_name, school, grade , total_points]
+        );
+        res.json(new_student.rows[0])
+    } catch(err) {
+        console.error(error);
+    }
+})
+
+app.patch('/teachers', async(req, res) => {
+    try {
+        const { full_name, school } = req.body;
+        const new_teacher = await pool.query("UPDATE teachers (full_name, school) VALUES ($1, $2) WHERE full_name = $1 RETURNING *",
+            [full_name, school]
+        );
+        res.json(new_teacher.rows[0])
+    } catch(err) {
+        console.error(error);
+    }
+})
+
+app.patch('/teachers/:teacher_id/classes', async(req, res) => {
+    try {
+        const teacher_id = parseInt(req.params.teacher_id);
+        const { class_name, class_code, class_colour, total_points } = req.body;
+        const new_class = await pool.query("UPDATE classes (class_name, class_code, class_colour, total_points, teacher_id) VALUES ($1, $2, $3, $4, $5) WHERE class_code = $5 RETURNING *",
+            [class_name, class_code, class_colour, total_points, teacher_id]
+        );
+        res.json(new_class.rows[0])
+    } catch(err) {
+        console.error(err);
+    }
+})
+
+app.patch('/classes/:class_id/assignments', async(req, res) => {
+    try {   
+        const class_id = parseInt(req.params.class_id)
+        const { assignment_name, assignment_details, due_date, assignment_points } = req.body;
+        const new_assignment = await pool.query(
+            "INSERT INTO assignments (assignment_name, assignment_details, due_date, assignment_points, class_id) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [assignment_name, assignment_details, due_date, assignment_points, class_id]
+        );
+        res.json(new_assignment.rows[0]);
+
+        const new_assignment_delegations = await pool.query(
+            `INSERT INTO assignment_delegation (assignment_id, student_id, status)
+            SELECT assignments.id, student_id, 'To Do'
+            FROM student_class_items
+            INNER JOIN assignments
+            ON student_class_items.class_id = assignments.class_id AND assignments.class_id = $1
+            RETURNING *
+            `, [class_id]
+        );
     } catch (err) {
         console.error(err);
     }
