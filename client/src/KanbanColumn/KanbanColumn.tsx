7c4a3d04ce41fe113a949f7ec0ea6e09f432db7f
Anoush <anoushras21@gmail.com>
2020-08-15 15:46:17 -0400

    Created filestructure for feature

---
+++ client/src/KanbanColumn/KanbanColumn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "./KanbanColumn.scss";
+
+
+export interface columnComponentProps {
+
+  name: string, 
+  className: string,
+  dueDate: string,
+  classColour: string 
+}
