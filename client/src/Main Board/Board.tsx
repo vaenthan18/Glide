6f34e49ebb3eccf5089be80ef87ab48b9ca10e47
Vaenthan Jeevarajah <vaenthan.j@gmail.com>
2020-08-16 22:07:39 -0400

    Switching frontend to class components

---
+++ client/src/Main Board/Board.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+    render() {
+        return (
+            <div className="col-sm">
+                <h1 className="display-4">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        );
+    }
+}
+
+export default Board;
