36ad820826c9f07710ba7b2a2895dee25bb00ff6
Brayden Royston <brayden.royston10@gmail.com>
2020-08-17 19:44:23 -0400

    Updated add assignment component

--- client/src/teacherAddAssignment/addAssignment.tsx
+++ client/src/teacherAddAssignment/addAssignment.tsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import './addAssignment.css';
+import 'react-bootstrap';
 
 class addAssignment extends Component {
     render() {
         return (
             <div className="col-sm" id="addAssignmentWrapper">
                 <h1 className="display-4">Add an Assignment</h1>
-                <p className="lead">Fill in the given fields below and select a class to create a new assignment.</p>
-                <div className="dropdown">
-                    <a className="dropdown-toggle" data-toggle="dropdown" href="#">Personal asset loans</a>
-                    <ul className="dropdown-menu" role="menu" aria-labelledby="dLabel">            
-                        <li><a href="#">asds</a></li>
-                        <li className="divider"></li>
-                    </ul>
-                </div>
+                <p id = "subtitle" className="lead">Specify which class this assignment is for.</p>
 
+                <input id = "assignmentField" className="form-control form-control-lg" type="text" placeholder="Class Name or Course Code"></input>
+
+                <p id = "subtitle" className="lead">Fill in the given fields below and select a class to create a new assignment.</p>
+                <input id = "assignmentField" className="form-control form-control-lg" type="text" placeholder="Assignment Name"></input>
+                <input id = "assignmentField" className="form-control form-control-lg" type="text" placeholder="Assignment Details"></input>
+                <input id = "assignmentField" className="form-control form-control-lg" type="text" placeholder="Due Date"></input>
+                <input id = "assignmentField" className="form-control form-control-lg" type="text" placeholder="Assignment Points"></input>
+                <button type="button" className="btn btn-primary">Add Assignment</button>
             </div>
         );
     }
